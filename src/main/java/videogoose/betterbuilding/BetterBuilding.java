a6e9d161e2a63f0d2b9dbead5e480d8d1f69b511
Garret Reichenbach <garretreichenbach@gmail.com>
2026-07-02 11:20:32 -0700

    yeet old mod

--- src/main/java/videogoose/betterbuilding/BetterBuilding.java
+++ src/main/java/videogoose/betterbuilding/BetterBuilding.java
@@ -1,14 +1,6 @@
 package videogoose.betterbuilding;
 
-import api.mod.StarLoader;
 import api.mod.StarMod;
-import videogoose.betterbuilding.command.CaptionCommand;
-import videogoose.betterbuilding.command.CaptureCommand;
-import videogoose.betterbuilding.command.GenTestCommand;
-import videogoose.betterbuilding.command.GenerateCommand;
-import videogoose.betterbuilding.command.IndexCommand;
-import videogoose.betterbuilding.command.RenderCommand;
-import videogoose.betterbuilding.command.SearchCommand;
 
 public class BetterBuilding extends StarMod {
 
@@ -27,16 +19,5 @@ public class BetterBuilding extends StarMod {
 	@Override
 	public void onEnable() {
 		instance = this;
-		registerCommands();
-	}
-
-	private void registerCommands() {
-		StarLoader.registerCommand(new GenTestCommand());
-		StarLoader.registerCommand(new CaptureCommand());
-		StarLoader.registerCommand(new RenderCommand());
-		StarLoader.registerCommand(new CaptionCommand());
-		StarLoader.registerCommand(new IndexCommand());
-		StarLoader.registerCommand(new SearchCommand());
-		StarLoader.registerCommand(new GenerateCommand());
 	}
 }
